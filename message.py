ad7f2bcedb690a5afb8b5f5159cdcbf5ca3a55b1
IBakuda <zoxspirit@gmail.com>
2024-09-21 08:37:45 +0300

    first commit

---
+++ message.py
@@ -0,0 +1,49 @@
+from datetime import datetime, date, time
+from json import load, dump
+
+
+class Message:
+    def __init__(self, d=datetime.now(), fileName='db.json'):
+        self.fileName = fileName
+        self.load()
+        self.dat = d.strftime("%y/%m/%d %H:%M")
+
+    def load(self):
+        try:
+            with open(self.fileName) as fp:
+                self.mess = load(fp)
+        except FileNotFoundError:
+            self.mess = {}
+
+    def save(self):
+        with open(self.fileName, 'w') as fp:
+            dump(self.mess, fp)
+
+    def add_message(self, titlee='Title', text='Text'):
+        title = titlee
+        self.mess[title] = {'date': self.dat, 'text': text}
+        self.save()
+
+    def remove_message(self, title):
+        if title in self.mess:
+            del self.mess[title]
+            self.save()
+
+    def get_messages(self):
+        return self.mess
+
+    def get_message(self, title):
+        return self.mess.get(title, None)
+
+
+a = Message()
+
+print(a.mess)
+
+# добавлять пользователя
+a.add_message("Poll", '124')
+a.add_message("gfdgdf", '231231')
+a.remove_message("gfdgdf")
+
+# Сохранить файл
+a.save()
